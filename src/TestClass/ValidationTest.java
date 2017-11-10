9f0b2e2d690ec3a48d75b57a4eabf12f602eb76d
faihasan <fai_hasan@hotmail.com>
2017-11-10 03:06:38 -0500

    11-10

--- src/TestClass/ValidationTest.java
+++ src/TestClass/ValidationTest.java
@@ -1,21 +1,116 @@
 package TestClass;
 
+import TestDemo1.*;
+
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.AfterMethod;
 
 public class ValidationTest {
-  @Test (priority = 0, enabled = true) 
+	  String sentence;
+	  String[] word;
+	  biggestWord bigWord;
+
+  @Test (priority = 0, enabled = true)   
   public void biggestWord(){
+	  
+	  bigWord = new biggestWord();
+	  sentence = "The cow jumped over the moon";
+	  
+	  word = bigWord.bWord(sentence);
+	  
+	  System.out.println("The Biggest Word is: "+word[0]);
+	  System.out.println("Word length is: "+word[1]);
+	  	  
   }
   
+ @Test (priority = 1, enabled = true)
+	 public void unitTesting1(){
+	 try {
+	 File src = new File("./TestCase.xlsx");
+		FileInputStream fis;
+		
+			fis = new FileInputStream(src);
+
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sh1 = wb.getSheet("Sheet1");
+		String testSentence = sh1.getRow(1).getCell(0).getStringCellValue();
+		
+		bigWord = new biggestWord();
+		word = bigWord.bWord(testSentence);
+		
+		String expectedResult01 = sh1.getRow(1).getCell(1).getStringCellValue();
+		String actualResult01 = word[0];
+		
+
+		
+		if(actualResult01.equals(expectedResult01)){
+
+			System.out.println(actualResult01+ "\n"+ "Test Passed");
+			
+		}
+		else{
+
+			System.out.println(actualResult01+ "\n"+ "Test Failed");
+		}
+		
+		wb.close();
+
+	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		 
+	 }
+ 
+ @Test (priority = 2, enabled = true)
+ public void unitTesting2(){
+	 try {
+	 File src = new File("./TestCase.xlsx");
+		FileInputStream fis;
+		
+			fis = new FileInputStream(src);
+
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sh1 = wb.getSheet("Sheet1");
+		String testSentence = sh1.getRow(2).getCell(0).getStringCellValue();
+		
+		bigWord = new biggestWord();
+		word = bigWord.bWord(testSentence);
+		
+		String expectedResult01 = sh1.getRow(2).getCell(1).getStringCellValue();
+		String actualResult01 = word[0];
+		
+
+		
+		if(actualResult01.equals(expectedResult01)){
+
+			System.out.println(actualResult01+ "\n"+ "Test Passed");
+			
+		}
+		else{
+
+			System.out.println(actualResult01+ "\n"+ "Test Failed");
+		}
+		
+		wb.close();
+
+	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		 
+	 }
+ }
   
-  @BeforeMethod
-  public void beforeMethod() {
-  }
+  
 
-  @AfterMethod
-  public void afterMethod() {
-  }
-
-}
+  
--- src/TestDemo1/biggestWord.java
+++ src/TestDemo1/biggestWord.java
@@ -2,9 +2,28 @@ package TestDemo1;
 
 public class biggestWord {
 	
-	public void biggestWord(String sentence){
+	public String[] bWord(String sentence){
+		String bigW = null;
+		int wordCount = 0;
+		int big = 0;
+		String[] result = new String[2];
 		
+		String[] bigWord = sentence.split(" ");
+		wordCount = bigWord.length;
 		
+		for(int i=0; i<wordCount; i++){
+			if(big < bigWord[i].length()){
+				bigW = bigWord[i];
+				big = bigWord[i].length(); 
+			}			
+			
+			
+		}
+		
+		result[0] = bigW;
+		result[1] = Integer.toString(big);
+		
+		return result;
 		
 		
 	}
